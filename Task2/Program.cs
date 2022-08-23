063b02d03278f6201cbdb21669937664a6ae6e13
Olga <lelya.zy@gmail.com>
2022-08-23 14:37:49 +0300

    New task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите первое число:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == y)
+{
+    Console.Write("Числа равны");
+}
+if (x > y)
+{
+    Console.Write("Число " + x + " больше, чем число " + y);
+}
+if (x < y)
+{
+    Console.Write("Число " + y + " больше, чем число " + x);
+}
